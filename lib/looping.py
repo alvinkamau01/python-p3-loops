f627bc3ec20d877daacf27620178dbbb553d247b
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-09-04 14:46:13 +0300

    .

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count_down=10
+    
+    while count_down>0:
+            print(count_down)
+            count_down-=1
+            if (count_down==0):
+                  print('Happy New Year!')
+   
 
+            
 def square_integers(int_list):
-    # code goes here!
-    pass
+      int_list_squared=[int_list_v**2 for int_list_v in int_list ]
+      return (int_list_squared)
+
+
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+            if i%3==0 and i%5==0:
+                  print ('FizzBuzz')
+            elif i%3==0:
+                  print ('Fizz')
+            elif i%5==0:
+                  print ('Buzz')
+            else: 
+                 print (i)
+            
+      
